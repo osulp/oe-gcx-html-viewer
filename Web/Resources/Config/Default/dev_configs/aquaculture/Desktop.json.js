303c88d1314823f95600018368a1c41febb2f729
Marc Rempel <rempelm@oregonstate.edu>
2020-08-26 15:23:57 -0700

    Added fix for search to workflow to put a 1 sec timeout on load to sync dom.

--- Web/Resources/Config/Default/dev_configs/aquaculture/Desktop.json.js
+++ Web/Resources/Config/Default/dev_configs/aquaculture/Desktop.json.js
@@ -7059,8 +7059,8 @@
                         "moduleName": "Site",
                         "moduleType": "geocortex.essentialsHtmlViewer.mapping.modules.site.SiteModule",
                         "configuration": {
-                            "siteUri": "http://tools.oregonexplorer.info/Geocortex/Essentials/oe/REST/sites/Aquaculture",
-                            "oeUri": "http://tools.oregonexplorer.info/Geocortex/Essentials/oe/REST/sites/__root",
+                            "siteUri": "https://tools.oregonexplorer.info/Geocortex/Essentials/oe/REST/sites/Aquaculture",
+                            "oeUri": "https://tools.oregonexplorer.info/Geocortex/Essentials/oe/REST/sites/__root",
                             "oeDevUri": "http://tools.oregonexplorer.info/Geocortex/Essentials/dev/REST/sites/dev_reg",
                             "cakeSiteUri": "http://tools.oregonexplorer.info/Geocortex/Essentials/oe/REST/sites/sfam_orwap_combined"
                         },
